5ee7f0f92fa48353e6cdf57f054fd2d4b924327e
sansan63kc <reagent-163-rw-08@yandex.ru>
2021-03-30 15:49:12 +0400

    использование componentDidUpdate

--- src/components/ProfilePage/ProfileInfo/ProfileStatus.jsx
+++ src/components/ProfilePage/ProfileInfo/ProfileStatus.jsx
@@ -26,6 +26,16 @@ class ProfileStatus extends React.Component {
         })       
     }
 
+    componentDidUpdate(prevProps, prevState){
+        debugger
+        if(prevProps.status!==this.props.status){
+            this.setState({
+                status:this.props.status
+            })
+        }
+        
+    }
+
     render(){
         return (
             <div>
