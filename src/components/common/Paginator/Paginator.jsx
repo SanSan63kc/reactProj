b57cb68763359040824b7ce712a4dbace85cd3fb
sansan63kc <reagent-163-rw-08@yandex.ru>
2021-04-27 21:50:19 +0400

    постраничный вывод не удался

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from "./Paginator.module.css";
 
-let Paginator =({totalUsersCount, pageSize, currentPage, onPageChanged})=>{
+let Paginator =({totalUsersCount, pageSize, currentPage, onPageChanged, portionSize=10})=>{
     
     let pagesCount=Math.ceil(totalUsersCount/pageSize)
 
@@ -10,15 +10,27 @@ let Paginator =({totalUsersCount, pageSize, currentPage, onPageChanged})=>{
     for (let i=1; i<=pagesCount; i++){
       pages.push(i)
     }
+
+    let portionCount=Math.ceil(pagesCount/portionSize)
+    let{portionNumber, setPortionNumber}=useState(1)
+    let leftPortionPageNumber=(portionNumber-1)*portionSize+1
+    let rightPortionNumber=portionNumber*portionSize
     
     return <div>
     <div className={classes.frendsPage}>
+      {portionNumber>1 &&
+      <button onClick={()=>{setPortionNumber(portionNumber-1)}}>PREV</button>}
         <div className={classes.usersList}>
             <div>
-                {pages.map(p=>{
+                {pages
+                .filter(p=>p>=leftPortionPageNumber && p<rightPortionNumber)
+                .map(p=>{
                 return <span className={currentPage===p && classes.selectedPage}
-                         onClick={(e)=>{onPageChanged(p)}}>{p}</span>})}
+                         key={p} onClick={(e)=>{onPageChanged(p)}}>{p}</span>
+                         })}       
             </div>
+            {portionCount>portionNumber &&
+            <button onClick={()=>{setPortionNumber(portionNumber+1)}}>Next</button>}
         </div>
     </div>
   </div>
