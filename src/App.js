6e42ef600738c4b6cc82e9d3b9e342ee27307a6c
sansan63kc <reagent-163-rw-08@yandex.ru>
2021-04-28 19:05:08 +0400

    react.lazy, suspense

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, {Component} from 'react'
+import React, {Component, Suspense}  from 'react'
 import { connect } from 'react-redux';
 import {BrowserRouter, Route, withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 import './App.css';
 import Preloader from './components/common/Preloader/Preloader';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import FrendsContainer from './components/Frends/FrendsContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import Music from './components/Music/Music';
 import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
-import ProfileContainer from './components/ProfilePage/ProfileContainer';
 import Settings from './components/Settings/Settings';
+import { withSuspense } from './hoc/withSuspense';
 import { initializeApp } from './redux/app-reducer';
+
+/* Ленивая загрузка компонент */
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const FrendsContainer = React.lazy(() => import('./components/Frends/FrendsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/ProfilePage/ProfileContainer'));
 
 class App extends Component{
   
@@ -35,13 +38,13 @@ class App extends Component{
           <div className="profile__page__content">
             
             <Route path="/profile/:userId?" 
-              render={()=><ProfileContainer/>}/>
+              render={withSuspense(ProfileContainer)}/>
             
             <Route path="/dialogs" 
-              render={()=><DialogsContainer  />}/>
+              render={withSuspense(DialogsContainer)}/>       
 
             <Route path="/frends" 
-              render={()=><FrendsContainer/>}/>
+              render={withSuspense(FrendsContainer)}/> 
 
             <Route path="/login" 
               render={()=><Login/>}/>
